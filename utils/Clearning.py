9995ac92f3c573d5cd2f4275c404160871d3761f
Fu Shang <31750484+firstkeaster@users.noreply.github.com>
2018-08-16 10:01:42 -0700

    Update Clearning.py

--- utils/Clearning.py
+++ utils/Clearning.py
@@ -17,12 +17,13 @@ import yaml
 
 nowpath = os.path.abspath('.')
 
+
 class Data_Cleaner(object):
     def cat_col_detector(self, df, in_cols, thre, df_test=None): 
-        #To detect categorical columns, if unique>thre, define as categorical.
+        ## To detect categorical columns, if unique>thre, define as categorical.
         cat_cols = []
         cat_col_cats = {}
-        #categorical columns and how many cats in each col
+        ## categorical columns and how many cats in each col
         for i in in_cols:
             num_cats = len(df[i].unique())
             if num_cats < thre:
@@ -31,7 +32,7 @@ class Data_Cleaner(object):
         return(cat_cols, cat_col_cats)
 
     def nan_dummy_getter(self, df, in_cols, df_test=None):
-        #Get dummies for Nan, out_cols to record dummy columns
+        ## Get dummies for Nan, out_cols to record dummy columns
         out_cols = []
         for i in in_cols:
             if (df[i].isnull().any().any() or df_test[i].isnull().any().any()):
@@ -42,7 +43,7 @@ class Data_Cleaner(object):
         return(df, df_test, out_cols)
 
     def categorical_encoder(self, df, in_cols, df_test=None):
-        ##Convert a list of categorical columns into int, range(len(unique))
+        ## Convert a list of categorical columns into int, range(len(unique))
         for col in in_cols:
             value_list = df[col].unique().tolist()
             df[col] = df[col].apply(lambda x: value_list.index(x))
@@ -50,9 +51,10 @@ class Data_Cleaner(object):
                 df_test[col] = df_test[col].apply(lambda x: value_list.index(x))
         cat_cols = in_cols
         return(df, df_test, cat_cols)
-
+    ##!!!!!!!!!!!Add infinity handeler
+    
     def dummy_getter(self, df, in_cols, drop_first=True, dummy_na=True, df_test=None):
-        ##get dummy, one dummy for Nan and drop first dummies
+        ## get dummy, one dummy for Nan and drop first dummies
         if df_test is not None:
             tem = pd.concat([df[in_cols],df_test[in_cols]], axis=0)
         elif df_test is None:
@@ -68,8 +70,9 @@ class Data_Cleaner(object):
             [df.shape[0]:tem_dummies.shape[0]]], axis=1)          
         return(df,df_test,out_cols)
 
-    ##fillin Na with mean/median/-999/zero
     def nan_filler(self, df, in_cols, method='median', df_test=None):
+        ## !!!!!!!!!!!!!!!Categorical
+        ## fillin Na with mean/median/-999/zero
         if type(method) != type('str'):
             switcher = {method: (lambda df,col: method)}
         else:
@@ -84,9 +87,9 @@ class Data_Cleaner(object):
                 df_test[i] = df_test[i].fillna(switcher[method](df,i))
         return(df,df_test)
 
-    def normalizer(self, df, in_cols, method='median', measure='range', df_test=None, \
-    inplace=True):
-        ##Normalization, keep consistency between train & test
+    def normalizer(self, df, in_cols, method='median', measure='range', \
+                    df_test=None, inplace=True):
+        ## Normalization, keep consistency between train & test
         switcher = {
             'median': (lambda df,col: df[col].median()),
             'mean': (lambda df,col: df[col].mean()),
@@ -113,9 +116,6 @@ class Data_Cleaner(object):
                     df_test[i+'_norm'] = (df_test[i] - referer)/denom
                 else:
                     df_test[i] = (df_test[i] - referer)/denom
-
-            
-            
         return(df,df_test)
 
 
